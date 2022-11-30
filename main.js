255e619012cce0f7d2b1601c6f42e05fe6f96a3c
hanguyen2002v <109323029+hanguyen2002v@users.noreply.github.com>
2022-11-30 14:21:01 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+///////////////////////////////////
+
+
+app.set('view engine', 'hbs')
+
+app.get('/'), ((req, res) =>{
+    const n = new Date().toLocaleDateString()
+    console.log("Hello: " + n)
+    res.render('home',{now:n})
+})
+
+//
+const PORT = process.env.PORT || 3000
+app.listen(PORT)
+console.log("My app is up!")
